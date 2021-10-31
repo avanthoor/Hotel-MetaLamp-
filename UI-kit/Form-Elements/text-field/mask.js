c90c39a2a085aba91415a609770cddc9065f6a21
avanthoor <92603778+avanthoor@users.noreply.github.com>
2021-10-31 15:06:43 +0300

    Реализовано условие установки курсора в начале маски

--- UI-kit/Form-Elements/text-field/mask.js
+++ UI-kit/Form-Elements/text-field/mask.js
@@ -1,1 +1,18 @@
- $("#date").mask("99.99.9999");
+//jQuery-плагин для установки курсора в определенной позиции
+$.fn.setCursorPosition = function(pos) {
+    if ($(this).get(0).setSelectionRange) {
+      $(this).get(0).setSelectionRange(pos, pos);
+    } else if ($(this).get(0).createTextRange) {
+      var range = $(this).get(0).createTextRange();
+      range.collapse(true);
+      range.moveEnd('character', pos);
+      range.moveStart('character', pos);
+      range.select();
+    }
+  };
+
+  //Добавляем маску с курсором в начале поля ввода
+  $("#date").click(function(){
+    $(this).setCursorPosition(0);
+  }).mask("99.99.9999");
+ 
