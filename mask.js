eb3bfa035b35e2c81a39d7c7f5537d9650688200
avanthoor <92603778+avanthoor@users.noreply.github.com>
2021-12-08 00:20:34 +0300

    refactorig

--- mask.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function() {
-    $("#date").mask("99/99/9999");
-});
-
-console.log("Text")
---
+++ src/index.js
@@ -0,0 +1,13 @@
+/* const omniImport = resolve => resolve.keys().forEach(resolve);
+
+omniImport( require.context('../src/', true, /\.js$|\.scss$/) ); */
+import './theme/global.scss'
+import './theme/vars.scss'
+
+import './components/text-field/text-field.scss'
+import './components/text-field/__dropdown/text-field__dropdown.scss'
+import './components/btn/btn.scss'
+
+import './components/text-field/mask.js'
+import './components/text-field/__dropdown/text-field__dropdown.js'
+
